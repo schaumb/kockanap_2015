0af693410e1a6324704c4dacd881f4dab68a4030
Komonyi <komonyilaszlo@gmail.com>
2015-11-06 18:07:36 +0100

    Bash

--- mellek/speed_of_power/cucc.sh
+++ mellek/speed_of_power/cucc.sh
@@ -1,26 +1,25 @@
-#!bin/bash
-
+#!/bin/bash
 read c
 read d
 read a
 read b
 while [ $a -ne $c -o $b -ne $d ]; do
 s=""
-if [ "$a"<"$c" ]; then
-s="$sS"
+if [ "$a" -lt "$c" ]; then
+s="${s}S"
 a=$((a+1))
 fi
-if [ $a>$c ]; then
-s="$sN"
+if [ "$c" -lt "$a" ]; then
+s="${s}N"
 a=$((a-1))
 fi
-if [ $b<$d ]; then
-s="$sE"
+if [ "$b" -lt "$d" ]; then
+s="${s}E"
 b=$((b+1))
 fi
-if [ $b>$d ]; then
-s="$sW"
+if [ "$d" -lt "$b" ]; then
+s="${s}W"
 b=$((b-1))
 fi
-echo $s
+echo "$s"
 done
